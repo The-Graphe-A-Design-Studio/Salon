16102c238036648bcff78985420a2252946f45ba
unknown <rohit1995sgh@gmail.com>
2020-08-12 12:31:48 +0530

    checking number exists or not

--- branch/processing/curd_form.php
+++ branch/processing/curd_form.php
@@ -2,33 +2,6 @@
 
     include("../../dbcon.php");
 
-    // if(isset($_POST['newcustName']) && isset($_POST['newcustEmail']) && isset($_POST['newcustPhone']) && isset($_POST['branch_id']))
-    // {
-    //     date_default_timezone_set("Asia/Qatar");
-    //     $order_time = date('h:i A');
-    //     $order_date = date('d M, Y');
-
-    //     function generateRandomString($length = 7)
-    //     {
-    //         $characters = 'aAbBc0CdDeE1fFgGh2HiIjJ3kKlLm4MnNoO5pPqQr6RsStT7uUvVw8WxXyY9zZ';
-    //         $charactersLength = strlen($characters);
-    //         $randomString = '';
-    //         for ($i = 0; $i < $length; $i++)
-    //         {
-    //             $randomString .= $characters[rand(0, $charactersLength - 1)];
-    //         }
-    //         return $randomString;
-    //     }
-
-    //     $code = generateRandomString();
-
-    //     mysqli_query($link, "insert into customers (c_code, branch_id, c_name, c_email, c_phone, c_date) values ('$code', '".$_POST['branch_id']."', 
-    //         '".$_POST['newcustName']."', '".$_POST['newcustEmail']."', '".$_POST['newcustPhone']."', '$order_date')");
-
-    //     echo "New customer registered";
-
-    // }
-
     if(isset($_POST['newcustName']) && isset($_POST['newcustTicket']) && isset($_POST['newcustPhone']) && isset($_POST['branch_id']))
     {
         $count = count($_POST['services']);
@@ -51,32 +24,43 @@
 
         $code = generateRandomString();
 
-        $insert_cust = mysqli_query($link, "insert into customers (c_code, branch_id, c_name, c_ticket, c_phone, c_date) values ('$code', '".$_POST['branch_id']."', 
-            '".$_POST['newcustName']."', '".$_POST['newcustTicket']."', '".$_POST['newcustPhone']."', '$order_date')");
-
-        if($insert_cust)
+        $sqlr = "SELECT * FROM cust_name_phone where cust_phone = '".$_POST['newcustPhone']."'";
+        $checkr = mysqli_query($link, $sqlr);
+        $rowr = mysqli_fetch_array($checkr, MYSQLI_ASSOC);
+        $count = mysqli_num_rows($checkr);
+        if($count >= 1)
         {
-            for ($i=0; $i < $count ; $i++)
-            {
-                $cat = "select * from services where se_id = '".$_POST['services'][$i]."'";
-                $get_cat = mysqli_query($link, $cat);
-                $cat_row = mysqli_fetch_array($get_cat, MYSQLI_ASSOC);
-
-                $sql = "insert into review_form (c_code, l_id, s_id, se_id, st_id) values ('$code', '".$_POST['branch_id']."', 
-                        '".$cat_row['s_id']."', '".$_POST['services'][$i]."', '".$_POST['staff'][$i]."')";
-                mysqli_query($link,$sql);
-            }
-
-            $sql2 = "update customers set c_status = '2' where c_code = '$code'";
-            mysqli_query($link, $sql2);
-
-            mysqli_query($link, "insert into cust_name_phone (cust_name, cust_phone) values ('".$_POST['newcustName']."', '".$_POST['newcustPhone']."')");
-            
-            echo "Customer registered and form created";
+            echo "This Phone Number is already registered";
         }
         else
         {
-            echo "Something went wrong. Try again";
+            $insert_cust = mysqli_query($link, "insert into customers (c_code, branch_id, c_name, c_ticket, c_phone, c_date) values ('$code', '".$_POST['branch_id']."', 
+            '".$_POST['newcustName']."', '".$_POST['newcustTicket']."', '".$_POST['newcustPhone']."', '$order_date')");
+
+            if($insert_cust)
+            {
+                for ($i=0; $i < $count ; $i++)
+                {
+                    $cat = "select * from services where se_id = '".$_POST['services'][$i]."'";
+                    $get_cat = mysqli_query($link, $cat);
+                    $cat_row = mysqli_fetch_array($get_cat, MYSQLI_ASSOC);
+
+                    $sql = "insert into review_form (c_code, l_id, s_id, se_id, st_id) values ('$code', '".$_POST['branch_id']."', 
+                            '".$cat_row['s_id']."', '".$_POST['services'][$i]."', '".$_POST['staff'][$i]."')";
+                    mysqli_query($link,$sql);
+                }
+
+                $sql2 = "update customers set c_status = '2' where c_code = '$code'";
+                mysqli_query($link, $sql2);
+
+                mysqli_query($link, "insert into cust_name_phone (cust_name, cust_phone) values ('".$_POST['newcustName']."', '".$_POST['newcustPhone']."')");
+                
+                echo "Customer registered and form created";
+            }
+            else
+            {
+                echo "Something went wrong. Try again";
+            }
         }
     }
     elseif(isset($_POST['exCustomer']) && isset($_POST['exTicket']) && isset($_POST['exPhone']) && isset($_POST['branch_id']))
