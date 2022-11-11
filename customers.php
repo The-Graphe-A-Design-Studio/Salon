a896f88a4755bada9dc460e9eeee6f695e6485c3
rohitsgh1995 <rohit1995sgh@gmail.com>
2022-11-11 19:58:56 +0530

    all customers page admin panel

--- customers.php
+++ customers.php
@@ -10,6 +10,88 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <title>Customers | diva lounge spa</title>
     <?php echo $head_tags; ?>
+    <style>
+        
+        table { 
+        width: 100%; 
+        border-collapse: collapse; 
+        }
+
+        th { 
+            background: #a5ce77; 
+            color: #f4f6f9; 
+            font-weight: bold;
+            }
+
+        td, th { 
+            padding: 8px; 
+            border: 1px solid #a5ce77; 
+            text-align: center; 
+            font-size: 1.1em;
+            }
+
+        /* 
+        Max width before this PARTICULAR table gets nasty
+        This query will take effect for any screen smaller than 760px
+        and also iPads specifically.
+        */
+        @media 
+        only screen and (max-width: 760px),
+        (min-device-width: 768px) and (max-device-width: 1024px)  {
+
+            table { 
+                width: 100%; 
+            }
+
+            /* Force table to not be like tables anymore */
+            table, thead, tbody, th, td, tr { 
+                display: block; 
+            }
+            
+            /* Hide table headers (but not display: none;, for accessibility) */
+            thead tr { 
+                position: absolute;
+                top: -9999px;
+                left: -9999px;
+            }
+            
+            tr { border: 1px solid #ccc; }
+            
+            td { 
+                /* Behave  like a "row" */
+                border: none;
+                border-bottom: 1px solid #eee; 
+                position: relative;
+                padding-left: 30%; 
+            }
+
+            .copy-td
+            {
+                padding-left: 0% !important;
+            }
+
+            .form-link
+            {
+                padding-left: 0 !important;
+            }
+
+            td:before { 
+                /* Now like a table header */
+                position: absolute;
+                /* Top/left values mimic padding */
+                top: 6px;
+                left: 6px;
+                padding-right: 10px; 
+                white-space: nowrap;
+                /* Label the data */
+                content: attr(data-column);
+
+                color: #000;
+                font-weight: bold;
+            }
+
+        }
+    </style>
 </head>
 <body>
     <div id="app">
@@ -22,33 +104,53 @@
         <div class="main-content">
             <section class="section">
                 <div class="section-header">
-                    <h1>Customers <span style="font-size: 0.6em; margin-left: 10px;">Feedback</span></h1>
+                    <h1>Customers</h1>
                     <div class="section-header-breadcrumb">
                         <div class="breadcrumb-item active"><a href="dashboard">Dashboard</a></div>
-                        <div class="breadcrumb-item">Cutomers Feedback</div>
+                        <div class="breadcrumb-item">Cutomers</div>
                     </div>
                 </div>
                 <div class="section-body">
 
                     <div class="row">
-                        <div class="col-12 col-md-12">
-                            <div class="custom-switches-stacked mt-2" style="flex-direction: row">
-                                <label class="custom-switch">
-                                    <input type="radio" name="option" class="custom-switch-input common_selector active" value="1">
-                                    <span class="custom-switch-indicator"></span>
-                                    <span class="custom-switch-description">Reviewed</span>
-                                </label>
-                                <label class="custom-switch">
-                                    <input type="radio" name="option" class="custom-switch-input common_selector inactive" value="2">
-                                    <span class="custom-switch-indicator"></span>
-                                    <span class="custom-switch-description">Pending</span>
-                                </label>
+                        <div class="col-12 col-md-3">
+                            <div class="form-group">
+                                <label>Branch</label>
+                                <select class="form-control common_selector branch">
+                                    <option value="">All</option>
+                                    <?php
+                                        $branch = "select * from locations";
+                                        $get_branch = mysqli_query($link, $branch);
+                                        while($row_branch = mysqli_fetch_array($get_branch, MYSQLI_ASSOC))
+                                        {
+                                    ?>
+                                    <option value="<?php echo $row_branch['l_id']; ?>"><?php echo $row_branch['l_name']; ?></option>
+                                    <?php } ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-3">
+                            <div class="form-group">
+                                <label>Name</label>
+                                <input type="text" class="form-control common_selector name" placeholder="Search by name">
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-3">
+                            <div class="form-group">
+                                <label>Start Date</label>
+                                <input type="date" placeholder="MM/DD/YYYY" class="form-control common_selector s_date" name="start_date"/>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-3">
+                            <div class="form-group">
+                                <label>End Date</label>
+                                <input type="date" placeholder="MM/DD/YYYY" class="form-control common_selector e_date" name="end_date"/>
                             </div>
                         </div>
                     </div>
-
-                    <div class="row mt-sm-4 filter_data">
                     
+                    <div class="row mt-sm-4 filter_data">
+
                     </div>
                 </div>
             </section>
@@ -68,31 +170,44 @@
             function filter_data()
             {
                 var action = 'fetch_data';
-                var active = get_filter('active');
-                var inactive = get_filter('inactive');
+                var branch = branchw();
+                var start_date = start_datee();
+                var end_date = end_datee();
+                // alert(branch);
+                // alert(start_date);
+                // alert(end_date);
                 $.ajax({
-                    url:"processing/curd_customer.php",
+                    url:"processing/curd_all_customers.php",
                     method:"POST",
-                    data:{action:action, active:active, inactive:inactive},
+                    data:{action:action, branch:branch, start_date:start_date, end_date:end_date},
                     success:function(data){
                         $('.filter_data').html(data);
                     }
                 });
             }
-        
-            function get_filter(class_name)
-            {
-                var filter = [];
-                $('.'+class_name+':checked').each(function(){
-                    filter.push($(this).val());
-                });
-                return filter;
-            }
-        
-            $('.common_selector').on('keyup change',function(){
-                filter_data();
-            });
 
+            function branchw()
+            {
+                return $('.branch').find('option:selected').val();
+            }
+
+            function start_datee()
+            {
+                return $('.s_date').val();
+            }
+
+            function end_datee()
+            {
+                return $('.e_date').val();
+            }
+
+            $('.common_selector').on('keyup change',function(){
+                // alert("jdhfj");
+                filter_data();
+                branchw();
+                start_datee();
+                end_datee();
+            });
 
             $(".customers").addClass("active");
         });
